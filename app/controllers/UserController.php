a447141cd5962463220e35974a232db461904821
Hachidaime <cold.kid40@gmail.com>
2020-09-17 12:42:46 +0700

    Update User: fix form method

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -70,14 +70,18 @@ class UserController extends Controller
      */
     public function form(int $id = null)
     {
-        list(, $count) = $this->userModel->singlearray($id);
-        if (!$count) {
-            Flasher::setFlash('Data tidak ditemukan!', $this->name, 'error');
-            header('Location: ' . BASE_URL . "/{$this->lowerName}");
-        }
-
         $tag = 'Tambah';
         if (!is_null($id)) {
+            list(, $count) = $this->userModel->singlearray($id);
+            if (!$count) {
+                Flasher::setFlash(
+                    'Data tidak ditemukan!',
+                    $this->name,
+                    'error',
+                );
+                header('Location: ' . BASE_URL . "/{$this->lowerName}");
+            }
+
             $tag = 'Ubah';
             $this->smarty->assign('id', $id);
         }
